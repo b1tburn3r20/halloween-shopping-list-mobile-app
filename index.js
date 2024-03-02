45288db6646c2081d8ac92c9bf507d8c688dd103
b1tburn3r20 <alejandrojosuebermudez@gmail.com>
2024-03-02 11:21:55 -0600

    updated ui and added variables to colors

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebas
 import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
 const appSettings = {
+    databaseURL : ""
 }
 
 const app =  initializeApp(appSettings)
